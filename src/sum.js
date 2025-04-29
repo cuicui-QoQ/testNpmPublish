51bedad938d6f5d60b7ad1a99efa652c60be389c
cuicui <wyg.scut@qq.com>
2025-04-29 17:00:44 +0800

    补充上测试覆盖率

--- src/sum.js
+++ src/sum.js
@@ -2,4 +2,11 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = sum
+function minus(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    sum,
+    minus
+}
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,6 @@
-const sum = require('../src/sum.js');
+const {sum, minus} = require('../src/sum.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+  expect(minus(1, 2)).toBe(-1);
 })
